b1b7e5d5b604bc4eb1eeb4839afa0f93b8cc017c
jaipur <yogendra.jain@allonblock.com>
2019-02-19 17:01:49 +0530

    added axios fetcher

---
+++ src/Global/dataFetch/axiosFetcher.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import generateV1uuid from '../Uuid';
+import {APPLICATION_BFF_URL} from '../../Redux/urlConstants'
+
+
+const axiosFetcher = ({method,url,reqObj,successCb,errorCb})=>
+{
+let requestObject = {};
+  requestObject.method = method;
+  requestObject.url = `${APPLICATION_BFF_URL}${url}`;
+
+  if (reqObj)
+    requestObject.data = {...reqObj};
+
+  requestObject.headers = {
+    originURL: window.origin,
+    correlationid: generateV1uuid(),
+    Authorization:`Bearer ${localStorage.getItem("Token")}`,
+    'Content-Type': 'application/json',
+  }
+  console.log(requestObject,"requestObject")
+
+  return axios(
+    requestObject
+  )
+    .then(responseData => {
+        successCb(responseData)
+    })
+.catch((err)=>
+{
+errorCb(err)
+})
+
+}
+export default axiosFetcher
