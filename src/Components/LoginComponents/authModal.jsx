2ed5a7172ae7aca91174e809f56c8016ef70cfe0
jaipur <yogendra.jain@allonblock.com>
2019-02-21 21:48:36 +0530

    added code for starting a session and authentication flow

---
+++ src/Components/LoginComponents/authModal.jsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import jwt_decode from 'jwt-decode';
+/* Lodash Imports */
+import _get from 'lodash/get';
+/* Material import */
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+/* Redux Imports */
+import connect from 'react-redux/lib/connect/connect';
+import genericPostData from '../../Global/dataFetch/genericPostData';
+/* Component Imports */
+
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
+class AuthModal extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            manager: '',
+            managerAuthList: []
+        }
+    }
+    componentDidMount() {
+        let url = 'StoreManager/ByStoreId'
+        genericPostData({
+            dispatch: this.props.dispatch,
+            url: 'StoreManager/ByStoreId',
+            reqObj: { id: localStorage.getItem('storeId') },
+            constants: {
+                init: 'GET_MANAGERES_LIST_INIT',
+                success: 'GET_MANAGERS_LIST_SUCCESS',
+                error: 'GET_MANAGERS_LIST_ERROR'
+            },
+            identifier: 'GET_MANAGERS_LIST',
+            successCb: this.handleManagerListSuccess,
+            errorCb: this.handleManagerListError
+        })
+    }
+    handleManagerListSuccess = (res) => {
+        debugger;
+        this.setState({ managerAuthList: res })
+    }
+    handleManagerListError = (error) => {
+        debugger;
+    }
+
+    handleLogin = () => {
+        let reqObj = {
+            id: this.state.manager,
+            pin: this.state.pin,
+        }
+        let url = 'StoreManager/ValidatePin';
+        genericPostData({
+            dispatch: this.props.dispatch,
+            url,
+            reqObj,
+            constants: {
+                init: 'POST_MANAGERE_LOGIN_INIT',
+                success: 'POST_MANAGER_LOGIN_SUCCESS',
+                error: 'POST_MANAGER_LOGIN_ERROR'
+            },
+            identifier: 'POST_MANAGER_LOGIN',
+            successCb: this.handleManagerLoginSuccess,
+            errorCb: this.handleManagerLoginError
+        })
+    }
+    handleManagerLoginSuccess = (data) => {
+        debugger;
+        if (data.result) {
+            this.props.authSuccess();
+            this.props.handleClose();
+            
+        }
+    }
+    handleManagerLoginError = (error) => {
+        debugger;
+    }
+
+    populateManagers = () => {
+        let managerAuthList = _get(this, 'state.managerAuthList', [])
+        let managerList = []
+        managerAuthList.map((data, index) => {
+            managerList.push(
+                <MenuItem value={data.id}>{data.person.firstName} {data.person.lastName}</MenuItem>
+            )
+        })
+        return (
+            <Select
+                value={this.state.manager}
+                onChange={this.handleChange}
+                input={
+                    <OutlinedInput
+                        labelWidth={this.state.labelWidth}
+                        name="manager"
+                        id="outlined-manager-simple"
+                    />
+                }
+            >
+                <MenuItem value=""><em>None</em></MenuItem>
+                {managerList}
+            </Select>
+        )
+    }
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleTextChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    render() {
+
+        return (
+            <Dialog
+                open={this.props.open}
+                TransitionComponent={Transition}
+                keepMounted
+                fullWidth
+                onClose={this.props.handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle id="alert-dialog-slide-title">
+                    {"Manager Auth Required"}
+                </DialogTitle>
+                <form onSubmit={this.handleSubmit}>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                            <FormControl variant="outlined" fullWidth>
+                                <InputLabel
+                                    ref={ref => {
+                                        this.InputLabelRef = ref;
+                                    }}
+                                    htmlFor="outlined-manager-simple"
+                                >Manager List</InputLabel>
+                                {
+                                    _get(this, 'state.managerAuthList', []).length > 0 ?
+                                        this.populateManagers() : null
+                                }
+                            </FormControl>
+                            <TextField
+                                id="pin"
+                                label="Pin"
+                                value={this.state.pin}
+                                onChange={this.handleTextChange('pin')}
+                                margin="outline"
+                                fullWidth
+                                type='password'
+                            />
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.props.handleClose} color="primary">
+                            Disagree
+                    </Button>
+                        <Button onClick={this.handleLogin} color="primary">
+                            Agree
+                    </Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {}
+}
+
+export default connect(mapStateToProps)(AuthModal);
--- src/Components/SessionComponents/DenominationDetailsForm.jsx
+++ src/Components/SessionComponents/DenominationDetailsForm.jsx
@@ -152,7 +152,84 @@ class DenominationDetailsForm extends React.Component {
 
     }
     startNewSession = ()=>{
+        let session = {};
+       let  openingCashDetails  = Object.keys(this.state).map((denomination)=>
+    {
+        let obj = {};
+        if (denomination == "pennies") {
+        obj.denomination = '0.01'
+        obj.quantity = parseInt(this.state[denomination])||0
+        obj.cashType = 1
+        }
+        else if ( denomination== "nickles") {
+            obj.denomination = '0.01'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 1
+        }
+        else if ( denomination== "dimes") {
+            obj.denomination = '0.1'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 1
+        }
+        else if ( denomination== "quaters") {
+            obj.denomination = '0.25'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 1
+        }
+        else if ( denomination== "fifyCent") {
+            obj.denomination = '0.5'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 1
+        }
+        else if ( denomination== "oneCoin") {
+            obj.denomination = '1'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 1
+        }
+        else if ( denomination== "one") {
+            obj.denomination = '1'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
 
+        }
+        else if ( denomination== "two") {
+            obj.denomination = '2'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        else if ( denomination== "five") {
+            obj.denomination = '5'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        else if ( denomination== "ten") {
+            obj.denomination = '10'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        else if ( denomination== "twenty") {
+            obj.denomination = '20'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        else if ( denomination== "fifty") {
+            obj.denomination = '50'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        else if (denomination== "hundred") {
+            obj.denomination = '100'
+            obj.quantity = parseInt(this.state[denomination])||0
+            obj.cashType = 0
+        }
+        return obj
+    })
+        session.terminalId = localStorage.getItem('terminalId');
+        session.storeId = localStorage.getItem('storeId');
+        session.operatorId = localStorage.getItem('userId');
+        session.openingCashDetails = openingCashDetails;
+        session.status = 'open';
+        session.openingBalance = {currencyCode:'$',amount:this.totalValue};
     }
     render() {
        let extraClass =  _get(this.props,'location.pathname','')=="/DenominationDetailsForm"?'session-parent':''
