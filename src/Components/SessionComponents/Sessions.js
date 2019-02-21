88928046f87f1794e61ede7360847ed2061b05bb
jaipur <yogendra.jain@allonblock.com>
2019-02-21 18:39:05 +0530

    added code for denomination dialog

--- src/Components/SessionComponents/Sessions.js
+++ src/Components/SessionComponents/Sessions.js
@@ -4,30 +4,29 @@ import _get from 'lodash/get';
 import _orderBy from 'lodash/orderBy';
 /* Material import */
 
-/* Redux Imports */ 
+/* Redux Imports */
 
 /* Component Imports */
 import Session from './Session';
 
 class Sessions extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            
+
         }
     }
-    
+
     render() {
-        let {sessionList} = this.props;
+        let { sessionList } = this.props;
         let sortedSessionList = _orderBy(sessionList, ['openingTimeStamp.seconds'], ['desc']);
         return (
-            <div>
-                {sortedSessionList.map((session,index)=><Session
+
+            <div >{sortedSessionList.map((session, index) => <Session
                 index={index}
                 session={session}
-                />)}
-            </div>
+            />)} </div>
         );
     }
 }
