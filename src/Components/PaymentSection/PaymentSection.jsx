3e41c87c708f8f207c03f78f99960a172e91565e
Marine Asalia <asalia.marine@gmail.com>
2019-02-23 15:39:53 +0530

    employee payment deduct button show flag change

--- src/Components/PaymentSection/PaymentSection.jsx
+++ src/Components/PaymentSection/PaymentSection.jsx
@@ -214,7 +214,7 @@ class PaymentSection extends React.Component {
                         <div onClick={this.handleCardPayment} className='each-payment-method'>
                             Debit/Credit Card
                         </div>
-                        {_get(this.props, 'customer.employee') ? <div onClick={this.handleDefaultCardPayment} className='each-payment-method'>
+                        {_get(this.props, 'customer.isEmpPayEnabled') ? <div onClick={this.handleDefaultCardPayment} className='each-payment-method'>
                             Employee
                         </div> : null}
                         {/* <div onClick={this.handleGiftCardPayment} className='each-payment-method'>
