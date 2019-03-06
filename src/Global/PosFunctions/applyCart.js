12c0ec61cb688520fba199af0d9fea4d0b827edc
jaipur <yogendra.jain@allonblock.com>
2019-03-06 21:30:25 +0530

    removed bug on hold cart item

--- src/Global/PosFunctions/applyCart.js
+++ src/Global/PosFunctions/applyCart.js
@@ -1,7 +1,7 @@
 import { commonActionCreater } from '../../Redux/commonAction';
 
 const applyCart = (dispatch, data)=>{
-    dispatch(commonActionCreater(data.cartDiscountAmount, 'ADD_DISCOUNT_TO_CART'));
+    dispatch(commonActionCreater(data.cartDiscountPercent, 'ADD_DISCOUNT_TO_CART'));
     dispatch(commonActionCreater(data.customer, 'ADD_CUSTOMER_TO_CART'));
     dispatch(commonActionCreater(data.cartItems, 'CART_ITEM_LIST')); // ! Always call in last
 }
