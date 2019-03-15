c0dab2dda49a7a6950d90b1a9899778a92fd469e
Marine Asalia <marine.asalia@allonblock.com>
2019-03-15 19:04:56 +0530

    changed upc code length criterion

--- src/Components/ProductsSection/ProductsSection.jsx
+++ src/Components/ProductsSection/ProductsSection.jsx
@@ -99,7 +99,7 @@ class ProductsSection extends React.Component {
                 console.log(err)
             });
         }
-        if (searchText.length == 12) {
+        if (searchText.length === 12 || searchText.length === 11 || searchText.length === 13) {
             let noSearchText = Number(searchText)
             productsdb.find({
                 selector: { "product.upcCode": noSearchText }
