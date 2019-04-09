c8c713df9ebe181af7f90fe3057badc24fa5b439
jaipur <yogendra.jain@allonblock.com>
2019-04-09 21:47:14 +0530

    added code for mapping summary

--- src/Components/Dialogues/HistoryDialogue.jsx/HistoryDetailArea.jsx
+++ src/Components/Dialogues/HistoryDialogue.jsx/HistoryDetailArea.jsx
@@ -19,9 +19,9 @@ class HistoryDetailArea extends React.Component {
     }
 
     showItemList = () => {
-        let saleItems = _get(this.props, "selectedSaleTransaction.sale.saleItems",[]);
+        let saleItems = _get(this.props, "selectedSaleTransaction.sale.saleItems", []);
         let saleItemResp = saleItems.map((saleItem) => {
-            return(<tr>
+            return (<tr>
                 <td>{_get(saleItem, "product.name", '')}</td>
                 <td>{_get(saleItem, "qty", 0)}</td>
                 <td>{_get(saleItem, "returnQty", 0)}</td>
@@ -34,6 +34,43 @@ class HistoryDetailArea extends React.Component {
 
         )
 
+    }
+    summaryPanel = () => {
+        let selectedOrder = _get(this.props, "selectedSaleTransaction", []);
+
+        let summaryPanelContent = <div className="mui-col-md-12" style={{ paddingRight: '50px' }}>
+             <label >{`Status: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'sale.totalTaxAmount.currencyCode', '$') + _get(selectedOrder, 'sale.totalTaxAmount.amount', 0)}</label>
+            <br />
+            <label >{`Created Date: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'sale.totalTaxAmount.currencyCode', '$') + _get(selectedOrder, 'sale.totalTaxAmount.amount', 0)}</label>
+            <br />
+            <label >{`Served By: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'operator.person.firstName', '') +' ' +_get(selectedOrder, 'operator.person.lastName', '')}</label>
+            <br />
+            <label >{`Tax: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'sale.totalTaxAmount.currencyCode', '$') + _get(selectedOrder, 'sale.totalTaxAmount.amount', 0)}</label>
+            <br />
+            <label >{`Grand Total: `}</label>
+            <label style={{ float: 'right' }}>{`${_get(selectedOrder, 'sale.totalAmount.currencyCode', '$')}${_get(selectedOrder, 'sale.totalAmount.amount', 0)}`}</label>
+            <br />
+            <label >{`Returned Amount: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'sale.totalRefundAmount.currencyCode', '$') + _get(selectedOrder, 'sale.totalRefundAmount.amount', '0')}</label>
+            <br />
+            <label >{`Total Paid: `}</label>
+            <label style={{ float: 'right' }}>{`${_get(selectedOrder, 'sale.totalAmountPaid.currencyCode', '$')}${_get(selectedOrder, 'sale.totalAmountPaid.amount', '0')}`}</label>
+            <br />
+            <label >{`Payment Method: `}</label>
+            <div style={{ float: 'right' }}>
+                {/* {this.showPaymentMethods(this.state.selectedOrder)} */}
+            </div>
+            {/* <label style={{ float: 'right' }}>{`$ ${_get(selectedOrder, 'sale.paymentAmount', '100.00')}`}</label> */}
+            <br />
+            <br />
+            <label >{`Change: `}</label>
+            <label style={{ float: 'right' }}>{_get(selectedOrder, 'sale.changeDue.currencyCode', 0) + _get(selectedOrder, 'sale.changeDue.amount', 0).toFixed(2)}</label>
+        </div>
+        return summaryPanelContent;
     }
 
     render() {
@@ -60,6 +97,7 @@ class HistoryDetailArea extends React.Component {
                     <div className='order-summary-section'>
                         <div className='card order-summary'>
                             <span className='card-title'>Summary</span>
+                            {this.summaryPanel()}
                         </div>
                         <div className='order-action-section flex-row'>
                             <div className='action-btn flex-row justify-center align-center'>Print</div>
@@ -69,7 +107,7 @@ class HistoryDetailArea extends React.Component {
                 </div>
                 <div className='refund-detail-section'>
                     <RefundHistory
-                    
+
                     />
                 </div>
 
