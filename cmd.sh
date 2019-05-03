8ff164e8c56fc290237aa2aa630421c11b338fdb
Mayuk <mayuk.agarwal@allonblock.com>
2019-05-03 11:15:05 +0530

    minor changes in build files

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,4 @@
 
 sed -i -e "s@##AOB_UI_BFF_URL##@$APPLICATION_BFF_URL@g" build/static/js/main.*.js
 sed -i -e "s@##AOB_UI_BFF_URL##@$APPLICATION_BFF_URL@g" build/static/js/main.*.js.map
-http-server build
+serve -s build
