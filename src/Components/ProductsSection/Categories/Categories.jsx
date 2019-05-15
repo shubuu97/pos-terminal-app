1a41a136a864ecc95002f3bb19d8f2be005da18f
Yogendra Jain <yogendra.jain@allonblock.com>
2019-05-15 19:03:33 +0530

    hot icon

--- src/Components/ProductsSection/Categories/Categories.jsx
+++ src/Components/ProductsSection/Categories/Categories.jsx
@@ -4,6 +4,8 @@ import BreadCrumb from "./BreadCrumb";
 // Lodash Import
 import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
+import _set from "lodash/set";
+
 //Redux Import
 import { commonActionCreater } from "../../../Redux/commonAction";
 import { connect } from "react-redux";
@@ -11,6 +13,8 @@ import { connect } from "react-redux";
 import PouchDb from "pouchdb";
 import Find from "pouchdb-find";
 import PAM from "pouchdb-adapter-memory"
+import genericPostData from '../../../Global/dataFetch/genericPostData';
+import Whatshot from '@material-ui/icons/Whatshot'
 PouchDb.plugin(Find);
 PouchDb.plugin(PAM);
 
@@ -68,10 +72,10 @@ class Categories extends Component {
   }
 
   handleHomeClick = () => {
-      this.props.getHomeClicked()
-      this.setState({ selectedCategory: {categoryType: -1} });
-      this.getProductData();
-      this.getCategory(0);
+    this.props.getHomeClicked()
+    this.setState({ selectedCategory: { categoryType: -1 } });
+    this.getProductData();
+    this.getCategory(0);
   };
 
   handleCategoryClick = category => {
@@ -111,12 +115,12 @@ class Categories extends Component {
       })
       .then(result => {
         let emptyResult
-        if(result.total_rows == 0) {
+        if (result.total_rows == 0) {
           emptyResult = {}
           this.props.dispatch(commonActionCreater(emptyResult, 'GET_PRODUCT_DATA_SUCCESS'))
         }
         result.pagination = {}
-        result.pagination.method="categories"
+        result.pagination.method = "categories"
         result.pagination.query = category.id
         result.pagination.fields = ["product.category1", "product.category2", "product.category3"]
         result.pagination.firstItemId = result.rows[0].id
@@ -159,7 +163,7 @@ class Categories extends Component {
       })
       .then(result => {
         result.pagination = {}
-        result.pagination.method="allDocs"
+        result.pagination.method = "allDocs"
         result.pagination.firstItemId = result.rows[0].id
         result.pagination.lastItemId = result.rows[result.rows.length - 1].id
         result.pagination.pageNo = 1
@@ -171,7 +175,44 @@ class Categories extends Component {
       })
       .catch(err => { });
   };
+  getHotProduct = () => {
+    genericPostData({
+      url: 'HotProducts/ByStore/Get',
+      dispatch: this.props.dispatch,
+      reqObj: { id: localStorage.getItem('storeId') },
+      identifier: 'HOT_PRODUCT_GET',
+      constants: {
+        init: 'HOT_PRODUCT_GET_LOGIN_INIT',
+        success: 'HOT_PRODUCT_GET_LOGIN_SUCCESS',
+        error: 'HOT_PRODUCT_GET_LOGIN_ERROR'
+      },
+      dontShowMessage: true,
+      successCb: this.hotProductGetResult
+    });
+  }
+  hotProductGetResult = (result) => {
+    debugger;
+    let products = _get(result, 'products', []).map((product) => {
+      let productt = { ...product }
+      _set(productt, 'id', productt.id);
+      _set(productt, 'doc', { product });
+      return productt;
 
+    })
+    this.setState({ hotProducts: products });
+    result.pagination = {}
+    result.pagination.method = "allDocs"
+    result.pagination.firstItemId = _get(result, 'products', [])[0].id
+    let length = _get(result, 'products', []).length
+    result.pagination.lastItemId = _get(result, `products$[${length - 1}].id`)
+    _set(result, 'rows', products);
+    result.pagination.pageNo = 1
+    result.pagination.startVal = 1
+    result.pagination.endVal = _get(result, 'products', []).length
+    this.props.dispatch(
+      commonActionCreater(result, "GET_PRODUCT_DATA_SUCCESS")
+    );
+  }
   render() {
     return (
       <div>
@@ -185,18 +226,24 @@ class Categories extends Component {
             selectedCurrentCategory={this.state.selectedCategory}
           />
         </div>
-        <div
-          className="product-catogories"
-        // style={{ height: this.props.categoriesHeight }}
-        >
-          {_get(this.state, "categoryToDisplay", []).map(category => {
-            return (
-              <Category
-                category={category}
-                clickHandler={this.handleCategoryClick}
-              />
-            );
-          })}
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div
+            style={{ width: '90%' }}
+            className="product-catogories"
+          // style={{ height: this.props.categoriesHeight }}
+          >
+            {_get(this.state, "categoryToDisplay", []).map(category => {
+              return (
+                <Category
+                  category={category}
+                  clickHandler={this.handleCategoryClick}
+                />
+              );
+            })}
+          </div>
+          <div onClick={this.getHotProduct} style={{ width: '10%' }}>
+            <Whatshot style={{ fontSize: '3em', color: 'red' }} />
+          </div>
         </div>
       </div>
     );
