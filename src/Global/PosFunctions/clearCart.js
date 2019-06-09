be33c8d4d0244dba38d5f5351561520f90591655
Mayuk <mayuk.agarwal@allonblock.com>
2019-06-09 13:23:38 +0530

    Clear cart bug

--- src/Global/PosFunctions/clearCart.js
+++ src/Global/PosFunctions/clearCart.js
@@ -7,6 +7,9 @@ const clearCart = (dispatch) => {
     cartDiscountObj.cartItems = []
     cartDiscountObj.prevCart = {}
     dispatch(commonActionCreater({}, 'ADD_CUSTOMER_TO_CART'));
+    if(localStorage.getItem('cannabisStore')){
+        dispatch(commonActionCreater({}, 'CUSTOMER_SERVING'));
+    }
     dispatch(commonActionCreater('', 'SALE_COMMENT'));
     dispatch(commonActionCreater(0, 'ADD_EMPLOYEE_DISCOUNT'));
     dispatch(commonActionCreater(cartDiscountObj, 'CART_ITEM_LIST')); // ! Always call in last
